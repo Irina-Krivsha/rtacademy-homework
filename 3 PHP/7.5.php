bf12976186a67413ae1663d1598aa375547fa156
Irina-Krivsha <irina.krivsha@gmail.com>
2021-08-18 21:52:43 +0300

    Homework 7.5

---
+++ 3 PHP/7.5.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ <meta charset="utf-8">
+ <title>7.5</title>
+</head>
+<body>
+
+<form action="" method="GET">
+    <label for="cityname">Місто</label>
+    <input type="text" id="cityname" name="city">
+
+    <button type="submit">Надіслати</button>
+</form>
+<?php
+$city = $_GET ['city'];
+echo($city)."\n\r";
+
+function cityname (string $city):string
+    {
+        $city=strip_tags($city);
+        $city=trim ($city);
+        $city=strtolower($city);
+        $city=ucfirst ($city);
+        return ($city);
+    }
+if ($city!=null)
+{
+    var_dump(cityname($city));
+}
+?>
+
+</body>
+</html>
